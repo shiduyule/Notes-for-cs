35de6fe750149e7ecc95b12ce9f061196533908b
shiduyule <shiduyule@163.com>
2022-12-06 22:52:10 +0800

    modify opencv coner.py

--- cs/python/opencv/opencv_tutorial-main/test06_corner.py
+++ cs/python/opencv/opencv_tutorial-main/test06_corner.py
@@ -1,13 +1,17 @@
 
 import cv2
-
-image = cv2.imread("opencv_logo.jpg")
+import matplotlib.pyplot as plt
+image = cv2.imread("c:/Users/shiduyule/Desktop/cv1.jpg")
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-corners = cv2.goodFeaturesToTrack(gray, 500, 0.1, 10)
+corners = cv2.goodFeaturesToTrack(gray, 2000, 0.0001, 0.01)
 for corner in corners:
     x, y = corner.ravel()
-    cv2.circle(image, (int(x), int(y)), 3, (255, 0, 255), -1)
+    cv2.circle(image, (int(x), int(y)), 1, (0, 0, 210), -1)
 
-cv2.imshow("corners", image)
-cv2.waitKey()
+image = image[:,:, ::-1]
+plt.imshow(image)
+plt.show()
+#cv2.imshow("corners", image)
+#cv2.waitKey()
+
