45d5a1f3131bf45b11b27859bf911592da9f612c
shiduyule in ubuntu <shiduyule@163.com>
2023-04-23 12:03:07 +0800

    complete lab1

---
+++ C-and-Cpp/lab1/exercises/print/print_0.1_0.2_0.3.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+    float a = 0.10;
+    float b = 0.20;
+    float c = 0.30;
+    
+    cout << "a = " << a << "\nb = " << b << "\nc = " << c << endl;  //  C++ 的标准输出语法 先写字符串文本  再写 变量名
+
+    return 0;
+}
